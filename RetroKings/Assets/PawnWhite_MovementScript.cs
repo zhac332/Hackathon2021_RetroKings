b1c3825d01da051423dac19b84381e056e663ec4
Stefan <zhac332@live.rhul.ac.uk>
2021-02-06 14:50:53 +0000

    Added an interface so that each piece implemented can be coded correctly.

--- RetroKings/Assets/PawnWhite_MovementScript.cs
+++ RetroKings/Assets/PawnWhite_MovementScript.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using Assets;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PawnWhite_MovementScript : MonoBehaviour
+public class PawnWhite_MovementScript : MonoBehaviour, PieceInterface
 {
 	private GameObject[,] board_cells = new GameObject[8, 8];
 	private int X_Coord = 0, Y_Coord = 0;
