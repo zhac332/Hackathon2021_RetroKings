0545cf06a260ee1c1e5eaa1cf1b6783d8303bb16
Stefan <zhac332@live.rhul.ac.uk>
2021-02-07 15:23:16 +0000

    Made the UI for the scene.

---
+++ RetroKings/Assets/ButtonsScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsScript : MonoBehaviour
+{
+    private GameManagerScript GameManager;
+
+	private void Start()
+	{
+		GameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+	}
+
+	public void ExtraTurnButton_Click()
+	{
+
+	}
+
+	public void ImmunityButton_Click()
+	{
+
+	}
+
+	public void KillButton_Click()
+	{
+
+	}
+
+	public void RestartButton_Click()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+}
