df705aa32f5849cceaa49f29d58b574dd07ef492
UWantMyName <stefantanasa13@gmail.com>
2024-02-20 19:06:56 +0200

    Prefabed the AreYouSure Panel and removed it from the scene, as I don't think it will be needed.

--- RetroKings/Assets/AreYouSurePanel.cs
+++ RetroKings/Assets/AreYouSurePanel.cs
@@ -11,18 +11,16 @@ public class AreYouSurePanel : MonoBehaviour
     [SerializeField] private AnimationClip ZoomOut;
     private Animator anim;
     private Action Yes_Trigger;
-    private Action No_Trigger;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    public void NewQuestion(string question, Action Yes_T, Action No_T)
+    public void NewQuestion(string question, Action Yes_T)
     {
         QuestionText.text = question;
         Yes_Trigger = Yes_T;
-        No_Trigger = No_T;
         anim.enabled = true;
         anim.Play(ZoomIn.name);
         GetComponent<Image>().raycastTarget = true;
@@ -45,6 +43,5 @@ public class AreYouSurePanel : MonoBehaviour
         anim.enabled = false;
         GetComponent<Image>().raycastTarget = false;
         if (yes) Yes_Trigger.Invoke();
-        else No_Trigger.Invoke();
     }
 }
